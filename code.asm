309d0b73da142b92a6130bac4599ac4292b2c432
LasCreations <lascellemckenzie@gmail.com>
2024-10-20 20:44:55 +0000

    Fix less more input

--- code.asm
+++ code.asm
@@ -11,8 +11,6 @@ _start:
     mov ax, cs      ; Load code segment into AX
     mov ds, ax      ; Copy code segment into data segment
 
-
-
     prompt_1:
         ; Print the first prompt (ENTER FIRST DIGIT)
         mov dx, p1      ; Load the address of p1 into DX
@@ -30,19 +28,24 @@ _start:
     check_1:
         cmp al, '9'
         JBE check_2   ;JBE jumps if the the value is less or equal to a value
+        ; If the input is greater than '9', print an error message
+        mov dx, error_message
+        mov ah, 09h     ; Function to print string
+        int 21h         ; Call DOS interrupt
         JMP prompt_1   ;brings back if the character is more  
 
 
     check_2:
         cmp al, '0'
-        JAE  check_2   ;JAE jumps if the the value is greater or equal to a value
-        JMP prompt_1
-
+        JAE  sub_val   ;JAE jumps if the the value is greater or equal to a value
+        mov dx, error_message
+        mov ah, 09h     ; Function to print string
+        int 21h         ; Call DOS interrupt
+        JMP prompt_1   ;brings back if the character is more 
 
     sub_val:
         sub al, 30h     ; Convert ASCII to number
         mov bl, al      ; Store the first digit in BL
-
 
     prompt_2:
         ; Print the second prompt (ENTER SECOND DIGIT)
@@ -84,3 +87,4 @@ section .data
     p1  db 10, 13, 'ENTER FIRST DIGIT: $'
     p2  db 10, 13, 'ENTER SECOND DIGIT: $'
     p3  db 10, 13, 'RESULT IS $'
+    error_message db 10, 13, 'INVALID INPUT TRY AGAIN $'
