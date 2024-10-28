33cc8432ad8ffab25759b5c0c84024b1931fe375
LasCreations <lascellemckenzie@gmail.com>
2024-10-28 21:43:31 +0000

    Menu added

--- code.asm
+++ code.asm
@@ -12,23 +12,88 @@ _start:
     mov ax, cs      ; Load code segment into AX
     mov ds, ax      ; Copy code segment into data segment
 
-    ;  Prompt and accept first digit
+    ;   Call Menu Option 
+    call menu
+    call readMenuOption
+
+    mov cl, al  ; Move option into CL
+
+    ; Prompt and accept first digit
     call prompt_user
     call read_input
-
-    mov bl, al  ;
+    mov bl, al  
 
     ; Prompt and accept second digit
     call prompt_user
     call read_input          
 
-    ; Add the two digits
-    add al, bl              ; Add first and second digit
+    cmp cl, '1'
+    call add_digits
+
+    ;cmp cl, '2'
+    ;call sub_digits
     
     cmp al, 9             ; Check if result is less/equal 9
     jbe single_digit_result 
     jmp double_digit_result ; else call
 
+readMenuOption:
+    ; Read first character
+    mov ah, 01h      ; Function to read a character
+    int 21h          ; Call DOS interrupt
+
+    ret
+    ;cmp al, '1'
+    ;call add_digits
+
+    ;cmp al, '2'
+    ;JE sub_digits
+
+    ;cmp al, 9             ; Check if result is less/equal 9
+    ;jbe single_digit_result 
+    ;jmp double_digit_result ; else call
+
+
+menu:
+    mov dx, menuopt
+    mov ah, 09h      
+    int 21h 
+
+    call newLine
+
+    mov dx, _add
+    mov ah, 09h      
+    int 21h 
+
+    call newLine
+
+    mov dx, _sub
+    mov ah, 09h      
+    int 21h 
+
+    call newLine
+
+    mov dx, _mul
+    mov ah, 09h      
+    int 21h 
+
+    call newLine
+
+    mov dx, exit
+    mov ah, 09h      
+    int 21h 
+
+    call newLine
+
+    ret
+
+add_digits:
+    add al, bl
+    ret
+    
+sub_digits:
+    sub al, bl
+    ret
 
 ; Prompt user to enter a digit
 prompt_user:
@@ -38,6 +103,7 @@ prompt_user:
     ret               
 
 read_input:
+
     ; Read first character
     mov ah, 01h      ; Function to read a character
     int 21h          ; Call DOS interrupt
@@ -71,14 +137,20 @@ single_digit_result:
     call exit_program
 
 
-double_digit_result :
-
+newLine:
     ;New Line
     mov ah, 02h
     mov dl, 13
     int 21h
     mov dl, 10
     int 21h 
+    ret
+
+
+double_digit_result :
+
+    ;New Line
+    call newLine
 
     ; Print the result message
     mov dx, result   
@@ -86,7 +158,7 @@ double_digit_result :
     int 21h          
 
 
-     ; For two-digit results, calculate tens and ones
+    ; For two-digit results, calculate tens and ones
     xor ah, ah        ; Clear AH to avoid issues with division
     mov dl, 10        ; Divisor (10)
     div dl            ; Divide AL by 10, quotient in AL (tens), remainder in AH (ones)
@@ -118,4 +190,9 @@ exit_program:
 section .data
     prompt           db 10, 13, 'ENTER DIGIT: $'
     result           db 10, 13, 'RESULT IS: $'
+    menuopt          db 10, 13, 'CHOOSE ARITHMETIC OPTION: $'
+    _add             db 10, 13, '1. ADDITION $'
+    _sub             db 10, 13, '2. SUBTRACTION $'
+    _mul             db 10, 13, '3. MULTIPLICATION $'
+    exit             db 10, 13, '4. EXIT $'
     error_message    db 10, 13, 'INVALID INPUT. TRY AGAIN.$'
